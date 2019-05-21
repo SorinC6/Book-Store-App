72dde7e00714da335415d77dd97f09c8967160c1
Sorin Chis <kish.sorin@yahoo.com>
2019-05-21 12:37:45 +0300

    remove node module, deployed server with heroku, replace  register and login endpoints

--- book-app/src/store/actions/registerActions.js
+++ book-app/src/store/actions/registerActions.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const REGISTER_START = 'REGISTER_START';
-export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
-export const REGISTER_FAILURE = 'REGISTER_FAILURE';
+export const REGISTER_START = "REGISTER_START";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAILURE = "REGISTER_FAILURE";
 
-export const register = (userData) => (dispatch) => {
-	dispatch({ type: REGISTER_START });
-	axios
-		.post('http://localhost:7111/api/register', userData)
-		.then((res) => {
-			localStorage.setItem('bookUser', userData.username);
-			dispatch({ type: REGISTER_SUCCESS, payload: userData.username });
-		})
-		.catch((err) => dispatch({ type: REGISTER_FAILURE, payload: err.message }));
+export const register = userData => dispatch => {
+  dispatch({ type: REGISTER_START });
+  axios
+    .post("https://backend-books.herokuapp.com/api/register", userData)
+    .then(res => {
+      localStorage.setItem("bookUser", userData.username);
+      dispatch({ type: REGISTER_SUCCESS, payload: userData.username });
+    })
+    .catch(err => dispatch({ type: REGISTER_FAILURE, payload: err.message }));
 };
